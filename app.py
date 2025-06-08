9c7512ade12af5ae09f63117588ebf7d98cf106b
Ryan Aleixo <thesaltyspoon321@gmail.com>
2025-06-08 00:36:18 -0400

    gunicorn

--- app.py
+++ app.py
@@ -47,43 +47,45 @@ def search():
 
     print("Accessed /get_data endpoint")        
 
-    query = f"""
+    query = """
         SELECT osuMaps.*, osuScores.*
         FROM osuMaps
         JOIN osuScores ON osuMaps.DiffID = osuScores.ID
-        WHERE osuScores.pp >= {input_list[0]} AND osuScores.pp <= {input_list[1]}
-        AND osuMaps.AR >= {input_list[2]} AND osuMaps.AR <= {input_list[3]}
-        AND osuMaps.OD >= {input_list[4]} AND osuMaps.OD <= {input_list[5]}
-        AND osuMaps.CS >= {input_list[6]} AND osuMaps.CS <= {input_list[7]}
-        AND osuMaps.SR >= {input_list[8]} AND osuMaps.SR <= {input_list[9]}
-        AND osuMaps.DrainTime >= {input_list[10]} AND osuMaps.DrainTime <= {input_list[11]}
-        AND osuScores.max_combo >= {input_list[12]} AND osuScores.max_combo <= {input_list[13]}
-        AND osuMaps.BPM >= {input_list[14]} AND osuMaps.BPM <= {input_list[15]}
-        AND (osuScores.accuracy >= {input_list[16]} AND osuScores.accuracy <= {input_list[17]})
-        AND CAST(osuMaps.MapsetPlaycount AS INTEGER) >= {input_list[18]} 
-        AND CAST(osuMaps.MapsetPlaycount AS INTEGER) <= {input_list[19]}
-        AND (
-            (osuScores.HD = {input_list[20]})
-            OR
-            ({input_list[20]} = 2)
-        )
-        AND (
-            (osuScores.HR = {input_list[21]})
-            OR
-            ({input_list[21]} = 2)
-        )
-        AND (
-            (osuScores.DT = {input_list[22]})
-            OR
-            ({input_list[22]} = 2)
-        )
-        AND (
-            (osuScores.FL = {input_list[23]})
-            OR
-            ({input_list[23]} = 2)
-        )
+        WHERE osuScores.pp >= ? AND osuScores.pp <= ?
+        AND osuMaps.AR >= ? AND osuMaps.AR <= ?
+        AND osuMaps.OD >= ? AND osuMaps.OD <= ?
+        AND osuMaps.CS >= ? AND osuMaps.CS <= ?
+        AND osuMaps.SR >= ? AND osuMaps.SR <= ?
+        AND osuMaps.DrainTime >= ? AND osuMaps.DrainTime <= ?
+        AND osuScores.max_combo >= ? AND osuScores.max_combo <= ?
+        AND osuMaps.BPM >= ? AND osuMaps.BPM <= ?
+        AND (osuScores.accuracy >= ? AND osuScores.accuracy <= ?)
+        AND CAST(osuMaps.MapsetPlaycount AS INTEGER) >= ? 
+        AND CAST(osuMaps.MapsetPlaycount AS INTEGER) <= ?
+        AND ((osuScores.HD = ?) OR (? = 2))
+        AND ((osuScores.HR = ?) OR (? = 2))
+        AND ((osuScores.DT = ?) OR (? = 2))
+        AND ((osuScores.FL = ?) OR (? = 2))
         ORDER BY DateRanked DESC;
     """
+
+    # Then pass the parameters
+    params = (
+        input_list[0], input_list[1],
+        input_list[2], input_list[3],
+        input_list[4], input_list[5],
+        input_list[6], input_list[7],
+        input_list[8], input_list[9],
+        input_list[10], input_list[11],
+        input_list[12], input_list[13],
+        input_list[14], input_list[15],
+        input_list[16], input_list[17],
+        input_list[18], input_list[19],
+        input_list[20], input_list[20],
+        input_list[21], input_list[21],
+        input_list[22], input_list[22],
+        input_list[23], input_list[23]
+    )
 
     cursor.execute(query)
     rows = cursor.fetchall()
